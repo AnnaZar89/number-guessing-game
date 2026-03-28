a97079a9552e0faea60af4029a1ae51c2b8f1879
Anna Zaryczanska <annazar00@gmail.com>
2026-03-28 09:37:24 +0100

    update: game logic and reset feature

--- app.py
+++ app.py
@@ -25,28 +25,35 @@ def start():
         num_of_guess = 5
     session["num_of_guess"] = num_of_guess
     session["computer_value"] = random.randint(1, 100)
+    session["difficulty_level"] = level_chosen
     return jsonify(level_chosen=level_chosen, num_of_guess=num_of_guess)
 
 
 @app.route("/guess", methods=['POST'])
+
 def guess():
     user_guess = int(request.form["guess"])
     computer_value = session["computer_value"]
     num_of_guess = session["num_of_guess"]
 
-    if computer_value == user_guess:
+    if user_guess <= 0:
+        message = "You lose. Game over!"
+
+    elif computer_value == user_guess:
         message = f"Guessed! {user_guess} is the right answer!"
     else:
         num_of_guess -= 1
         session["num_of_guess"] = num_of_guess
         if num_of_guess == 0:
-            message = "You lose. Game over!"
+            message = f"You lose! The number was {computer_value}. Game over!"
         elif user_guess > computer_value:
             message = "Too high."
         else:
             message = "Too low."
 
     return jsonify(message=message, num_of_guess=num_of_guess)
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
